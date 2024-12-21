b91131b5c297253d8cb2532974f32d7b8dee980f
Andreas Loizides <andreas16700@icloud.com>
2024-12-21 08:33:29 +0100

    Initial Commit

---
+++ Annotate/AnnotateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnotateApp.swift
+//  Annotate
+//
+//  Created by Andreas Loizides on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnnotateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
